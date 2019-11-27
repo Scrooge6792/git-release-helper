e1926de9baf383e5610d0a46681e50b0b5080df9
zyj <2335561220@qq.com>
2019-11-27 15:55:23 +0800

    ssh交互及部分细节

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const argv = process.argv.slice(2)
+const path = require('path')
+const chalk = require('chalk')
+
+if (argv.length > 1) {
+  console.log('Unknown command')
+} else if (argv.length === 0) {
+  require('./lib')()
+} else {
+  switch (argv[0]) {
+    case '-v':
+      console.log(require('./package').version)
+      break
+    case 'init':
+      require('fs').copyFile(
+        path.resolve('./lib', 'default.json'),
+        path.resolve(process.cwd(), 'grhconfig.json'),
+        (err) => {
+          if (err) {
+            throw err
+          }
+          console.log(chalk.greenBright('Successfully initialized'))
+        }
+      )
+      break
+    default:
+      console.log('Unknown command')
+  }
+}
